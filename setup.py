aa81b8df9ddfaacf7df0dd2ab2be068464774a67
balmaster <iam.balmaster@gmail.com>
2014-02-18 04:54:56 +0400

    fix package data

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
         'django (>1.1)',
     ],
     packages=['schedule_field'],
-    package_data={'schedule_field': ['fixtures/*']},
+    package_data={'schedule_field': ['fixtures/*','static/*','static/widgets/*','static/widgets/schedule/*']},
     zip_safe=False
 )
